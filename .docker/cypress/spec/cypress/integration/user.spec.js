c87a65264ff37a8cc59c467594b746799c2ffcd8
Carlo Beltrame <carlo@beltra.me>
2020-03-30 15:03:25 +0200

    Make transactional tests work

--- .docker/cypress/spec/cypress/integration/user.spec.js
+++ .docker/cypress/spec/cypress/integration/user.spec.js
@@ -34,4 +34,25 @@ describe('The user', function () {
 
     cy.contains("Rolle Geschäftsleiter für Marlies Sauerkraut / Brexit in Pfadibewegung Schweiz wurde erfolgreich erstellt.")
   })
+  it('can still add a new person, even with the same email', function () {
+    cy.visit('/de/groups/1/roles/new')
+
+    cy.get('a:contains("Neue Person erfassen")').click()
+
+    cy.get('#role_new_person_first_name')
+      .type('Marlies')
+    cy.get('#role_new_person_last_name')
+      .type('Sauerkraut')
+    cy.get('#role_new_person_nickname')
+      .type('Brexit')
+    cy.get('#role_new_person_email')
+      .type('marlies_sk88@example.com')
+
+    cy.get('#role_type_select').click()
+    cy.get('div.chosen-search input').type("Gesch{enter}")
+
+    cy.get('form#new_role button[type="submit"]:first').click()
+
+    cy.contains("Rolle Geschäftsleiter für Marlies Sauerkraut / Brexit in Pfadibewegung Schweiz wurde erfolgreich erstellt.")
+  })
 })
