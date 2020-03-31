5a0ddd60f5370e63894d96831de7759f429e02bd
Nicolas Da Mutten <nicolas@damutten.ch>
2020-03-31 18:16:05 +0200

    Finish that spec

--- .docker/cypress/spec/cypress/integration/user.spec.js
+++ .docker/cypress/spec/cypress/integration/user.spec.js
@@ -56,26 +56,34 @@ describe('The user', function () {
     cy.contains("Rolle Geschäftsleiter für Marlies Sauerkraut / Brexit in Pfadibewegung Schweiz wurde erfolgreich erstellt.")
   })
   it('can move a role', function () {
-    cy.visit("/de/groups/1/people/18")
-    cy.get('section.roles strong a[href]').click()
-    cy.get('section.roles').should('not.exist')
+    cy.visit("/de/groups/1/people/31")
+    cy.get("#content .content-header").contains("Debitis")
+    cy.get('section.roles').find(".btn").click()
+		cy.get('#role_type_select').click().find("input.chosen-search-input").type("PowerUser{enter}")
+    cy.get("#role_created_at").as("date-input").type("25.02.2020").closest("fieldset").find("legend").click()
+    cy.get(".bottom button[type=submit]").click()
 
-    cy.get('#content .content-header .nav li:nth-child(2) a[href*=people]').click()
+    cy.get("#flash").should('contain', "Rolle").and('contain', "wurde erfolgreich erstellt.")
+
+    cy.visit("/de/groups/1/people")
     cy.contains("Debitis").closest("tr").find("input[type=checkbox]").click()
     cy.get(".actions-enabled .actions a[href$=move]").click()
 
     cy.get("#role_type").select("Group::Bund::ItSupport")
-    cy.get("#role_created_at").as("date-input").type("25.04.2020").closest("fieldset").find("legend").click()
+    cy.get("#role_created_at").as("date-input").type("25.03.2020").closest("fieldset").find("legend").click()
     cy.get(".modal-footer button[type=submit]").click()
     cy.get("@date-input").should('not.be.visible')
+
     cy.get(".modal-footer button[type=submit]").click()
 
     cy.get("#flash").should('contain', "Eine Rolle wurde verschoben")
     cy.get("#quicksearch").as("quicksearch").type("Berner Mara Debitis")
     cy.get("@quicksearch").closest(".quicksearch").find(".dropdown-menu a[href]").first().click()
 
-    cy.get('.content-header > .nav > :nth-child(2) > a').click()
-    // History broken here…
+    cy.get('.content-header > .nav > li:nth-child(2) > a').as('history-tab').click()
+    cy.get('.content-header > .nav > li:nth-child(2)').should('have.class', "active")
+
+    cy.get("h2").contains("Rollen").next().contains("PowerUser").closest("tr").find("td:last-child").should("have.text", "25.03.2020")
 
   })
 })
