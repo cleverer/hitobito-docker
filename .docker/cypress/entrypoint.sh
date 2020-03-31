f97b740be12772c21cb80347ae055c741cf80797
Carlo Beltrame <carlo@beltra.me>
2020-03-31 13:18:28 +0200

    Add notice about long seeding time to the entrypoint

--- .docker/cypress/entrypoint.sh
+++ .docker/cypress/entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-echo "💤 Waiting for hitobito to be ready!"
+echo "💤 Waiting for hitobito to be ready! The first time, this can take a few minutes until the database is seeded."
 npx -q wait-on "${CYPRESS_BASE_URL}"
 echo "✅ Hitobito is ready!"
 
